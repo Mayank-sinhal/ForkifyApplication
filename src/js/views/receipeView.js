abeccd34cd42029b21279a2317f9ccce5cd9d8e1
Mayank-sinhal <sinhal.mayank.mk@gmail.com>
2023-06-08 00:01:36 +0530

    Fraction error change

--- src/js/views/receipeView.js
+++ src/js/views/receipeView.js
@@ -121,13 +121,12 @@ class RecipeView extends View {
   }
 
   _generateMarkipIngredent(ing) {
+    const value = new Fraction(ing.quantity).toString();
     return `<li class="recipe__ingredient">
       <svg class="recipe__icon">
         <use href="${icons}#icon-check"></use>
       </svg>
-      <div class="recipe__quantity">${
-        ing.quantity ? new Fraction(ing.quantity).toString() : ''
-      }</div>
+      <div class="recipe__quantity">${ing.quantity ? value : ''}</div>
       <div class="recipe__description">
         <span class="recipe__unit">${ing.unit}</span>
         ${ing.description}
